531cb943b82b4ba90657d2888e858022cc3239c8
uabua <mariami.chakhvadze.1@btu.edu.ge>
2020-08-07 13:18:28 +0400

    Add solution

---
+++ bioinformatics-stronghold/enumerating-gene-orders/perm.py
@@ -0,0 +1,26 @@
+"""
+My solution to Rosalind problem 'Enumerating Gene Orders'
+
+ID: PERM
+Title: Enumerating Gene Orders
+URL: http://rosalind.info/problems/perm/
+"""
+
+from math import factorial
+from itertools import permutations
+
+def print_permutation(num):
+    """
+    Prints the total number of permutations of length num, followed by a list of all such permutations.
+
+    Args:
+        num {int}: positive integer
+
+    Returns:
+        None
+    """
+    print(factorial(num))
+
+    for permutation in permutations(range(1, num+1)):
+        print(" ".join(map(str, permutation)))
+    
