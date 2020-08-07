06880587f415b3aedaaa68d551e9dadc52ed848a
uabua <mariami.chakhvadze.1@btu.edu.ge>
2020-08-07 15:06:33 +0400

    Add solution

---
+++ bioinformatics-stronghold/enumerating-k-mers-lexicographically/lexf.py
@@ -0,0 +1,24 @@
+"""
+My solution to Rosalind problem 'Enumerating k-mers Lexicographically'
+
+ID: LEXF
+Title: Enumerating k-mers Lexicographically
+URL: http://rosalind.info/problems/lexf/
+"""
+
+from itertools import product
+
+def lexicographic_cartesian_product(alphabet, length):
+    """
+    Prints all strings that can be formed from the alphabet, ordered lexicographically.
+
+    Args:
+        alphabet {str}: collection of symbols defining an ordered alphabet
+        length {int}: length of formed string
+
+    Returns:
+        None
+    """
+    for p in product(alphabet.replace(" ", ""), repeat=length):
+        print("".join(p))
+
